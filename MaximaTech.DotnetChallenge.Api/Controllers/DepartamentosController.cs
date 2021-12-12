5cb9d7b2ef9ebdf4790b4a49d02fb5bfc7e71837
Lucas Moraes <lucasdemoraesc@gmail.com>
2021-12-12 17:36:17 -0300

    Adiciona controller para departamentos

---
+++ MaximaTech.DotnetChallenge.Api/Controllers/DepartamentosController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaximaTech.DotnetChallenge.Api.Services.Interfaces;
+using MaximaTech.DotnetChallenge.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaximaTech.DotnetChallenge.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        private readonly IDepartamentoService _departamentoService;
+
+        public DepartamentosController(IDepartamentoService departamentoService)
+        {
+            _departamentoService = departamentoService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<Departamento>>> GetAllAsync()
+        {
+            var departamentos = await _departamentoService.ListAsync();
+
+            return departamentos == null || departamentos.Count() <= 0
+                ? NoContent() : Ok(departamentos);
+        }
+    }
+}
